61f545a675efeabfc26a9e6c852be18f555ff1ee
Renan Oliveira <renanlido@yahoo.com.br>
2017-12-19 20:37:27 -0200

    Atualizando dados do CSS E JS

---
+++ js/main.js
@@ -0,0 +1,7 @@
+//função para manter selecionado quando clicar
+$('.cliente-Lista').click(function(){
+    //remove class active
+    $('.cliente-Lista').removeClass('ativo');
+    //adiciona class active ao item clicado
+    $(this).addClass('ativo');
+});
