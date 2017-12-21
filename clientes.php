493404265df89691728dd8fb11308cd30f3d52ac
Renan Oliveira <renanlido@yahoo.com.br>
2017-12-21 16:15:17 -0200

    Atualização de JS e Criação do Modal

--- clientes.php
+++ clientes.php
@@ -76,9 +76,6 @@ if (isset($_POST['salvar'])){
         <aside>
             <!-- <a href="#modal-altera-cadastro" ><div class="altera-cadastro"></div></a> -->
             <form id="form-cliente" class="form-default">
-                <?php
-                //$id = $_GET['id'];
-                ?>
                 <div class="container-form1 container-form1-clientes">
                     <label for="nome">Nome ou Apelido:</label>
                     <input type="text" name="nome" id="nome" disabled/>
@@ -155,10 +152,10 @@ if (isset($_POST['salvar'])){
 
 
         <!--INICIO MODAL DE ALTERAÇÃO DE CADASTRO-->
-        <aside id="modal-altera-cadastro" class="modal-alteara-cadastro">
+        <aside id="modal-altera-cadastro" class="modal-altera-cadastro">
             <div class="modal-header">
                 <h1>Cliente
-                    <a href="?pagina=clientes" target="_self">X</a>
+                    <span id="">X</span>
                 </h1>
             </div>
             <div class="modal-topo">
@@ -257,97 +254,99 @@ if (isset($_POST['salvar'])){
         }
         ?>
 
-        <aside id="modal-novo-cliente">
-            <div class="modal-header">
-                <h1>Cliente
-                    <a href="?pagina=clientes" target="_self">X</a>
-                </h1>
-            </div>
-            <div class="modal-topo">
-                <h1>Novo Cliente</h1>
-            </div>
-
-            <!--INICIO FORM DE CADASTRO-->
-            <form class="form-default modal-form-default" method="POST" action="">
-                <div class="container-form1 container-form1-clientes">
-                    <label for="nome">Nome ou Apelido:</label>
-                    <input type="text" id="nome" name="cad_apelido"/>
-                    <label for="nome_compl">Nome Completo : </label>
-                    <input type="text" id="nome_compl" name="cad_nomeCompl"/>
-
-                    <label for="tel">Telefone: </label>
-                    <input type="tel" id="tel" name="cad_tel"/>
-
-                    <label for="cel" class="input60">Celular: </label>
-                    <input type="tel" id="cel" name="cad_cel"/>
-
-                    <label for="email">E-mail: </label>
-                    <input type="email" id="email" name="cad_email"/>
-
-                    <label for="niver">Aniversário: </label>
-                    <input type="date" id="niver" name="cad_niver"/>
+        <aside class="container_modal_novo_cliente" id="container_novo_cliente_modal">
+            <div class="modal-novo-cliente">
+                <div class="modal-header">
+                    <h1>Cliente
+                        <span id="close_modal">&times;</span>
+                    </h1>
+                </div>
+                <div class="modal-topo">
+                    <h1>Novo Cliente</h1>
                 </div>
 
-                <div class="container-form2">
-                    <label>Endereço: </label>
-                </div>
+                <!--INICIO FORM DE CADASTRO-->
+                <form class="form-default modal-form-default" method="POST" action="">
+                    <div class="container-form1 container-form1-clientes">
+                        <label for="nome">Nome ou Apelido:</label>
+                        <input type="text" id="nome" name="cad_apelido"/>
+                        <label for="nome_compl">Nome Completo : </label>
+                        <input type="text" id="nome_compl" name="cad_nomeCompl"/>
 
-                <div class="container-form3">
-                    <div class="container-form3-box">
-                        <label for="cep">CEP: </label>
-                        <input type="number" class="label93" id="cep" name="cad_cep"/>
-                        <br/>
+                        <label for="tel">Telefone: </label>
+                        <input type="tel" id="tel" name="cad_tel"/>
 
-                        <label for="end">Endereço: </label>
-                        <input type="text" id="end" name="cad_end"/>
-                        <br/>
+                        <label for="cel" class="input60">Celular: </label>
+                        <input type="tel" id="cel" name="cad_cel"/>
 
-                        <label for="num">Número: </label>
-                        <input type="number" class="input60" id="num" name="cad_num"/>
+                        <label for="email">E-mail: </label>
+                        <input type="email" id="email" name="cad_email"/>
 
-                        <label for="comp" class="label93">Complemento: </label>
-                        <input type="text" class="input209" id="comp" name="cad_compl"/>
-                        <br/>
+                        <label for="niver">Aniversário: </label>
+                        <input type="date" id="niver" name="cad_niver"/>
+                    </div>
 
-                        <label for="bairro">Bairro: </label>
-                        <input type="text" id="bairro" name="cad_bairro"/>
-                        <br/>
-                        <div class="container-uf-cidade">
-                            <div class="content-cidade">
-                                <label for="cidade">Cidade: </label>
-                                <input type="text" class="input260" id="cidade" name="cad_cidade"/>
-                                <br/>
-                            </div>
-                            <div class="content-uf">
-                                <label for="uf">Estado: </label>
-                                <select class="input60" id="uf" name="cad_uf">
-                                    <option value="---">---</option>
-                                    <option value="RJ">RJ</option>
-                                    <option value="SP">SP</option>
-                                    <option value="MG">MG</option>
-                                </select>
+                    <div class="container-form2">
+                        <label>Endereço: </label>
+                    </div>
+
+                    <div class="container-form3">
+                        <div class="container-form3-box">
+                            <label for="cep">CEP: </label>
+                            <input type="number" class="label93" id="cep" name="cad_cep"/>
+                            <br/>
+
+                            <label for="end">Endereço: </label>
+                            <input type="text" id="end" name="cad_end"/>
+                            <br/>
+
+                            <label for="num">Número: </label>
+                            <input type="number" class="input60" id="num" name="cad_num"/>
+
+                            <label for="comp" class="label93">Complemento: </label>
+                            <input type="text" class="input209" id="comp" name="cad_compl"/>
+                            <br/>
+
+                            <label for="bairro">Bairro: </label>
+                            <input type="text" id="bairro" name="cad_bairro"/>
+                            <br/>
+                            <div class="container-uf-cidade">
+                                <div class="content-cidade">
+                                    <label for="cidade">Cidade: </label>
+                                    <input type="text" class="input260" id="cidade" name="cad_cidade"/>
+                                    <br/>
+                                </div>
+                                <div class="content-uf">
+                                    <label for="uf">Estado: </label>
+                                    <select class="input60" id="uf" name="cad_uf">
+                                        <option value="---">---</option>
+                                        <option value="RJ">RJ</option>
+                                        <option value="SP">SP</option>
+                                        <option value="MG">MG</option>
+                                    </select>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
 
-                <div class="modal-container-form4">
-                    <div class="lab-2">
-                        <label for="obs">Observações: </label>
+                    <div class="modal-container-form4">
+                        <div class="lab-2">
+                            <label for="obs">Observações: </label>
+                        </div>
+                        <div class="modal-container-form-obs-tx">
+                            <textarea id="obs" name="cad_obs"></textarea>
+                            <br/>
+                        </div>
                     </div>
-                    <div class="modal-container-form-obs-tx">
-                        <textarea id="obs" name="cad_obs"></textarea>
-                        <br/>
-                    </div>
-                </div>
 
-                <div class="modal-footer">
-                    <div class="modal_content_button">
-                        <input type="submit" value="Salvar" name="salvar">
-                        <a href="?pagina=clientes" target="_self"><input type="button" value="Cancelar"></a>
+                    <div class="modal-footer">
+                        <div class="modal_content_button">
+                            <input type="submit" value="Salvar" name="salvar">
+                            <input type="button" id="btn_cancelar" value="Cancelar">
+                        </div>
                     </div>
-                </div>
-            </form>
+                </form>
+            </div>
         </aside>
         <!--FINALIZA MODAL DE NOVO CADASTRO-->
 
@@ -357,7 +356,7 @@ if (isset($_POST['salvar'])){
 <!--Botões-->
 <footer>
     <div class="btn_footer">
-        <a href="#modal-novo-cliente"><input type="button" name="novo" value="Novo"/></a>
+        <button id="novoClienteBTN">Novo</button>
         <a href="#modal-altera-cadastro"><input type="button" name="alterar" value="Alterar"/></a>
         <input type="button" name="excluir" value="Excluir"/>
         <input type="button" name="restaurar" value="Restaurar"/>
@@ -368,6 +367,36 @@ if (isset($_POST['salvar'])){
 
 
 <script>
+//Abrir e fechar modal
+var modalNovoCLiente = document.getElementById('container_novo_cliente_modal');
+var novoClienteBtn= document.getElementById('novoClienteBTN');
+var closeModal= document.getElementById('close_modal');
+var btnCancelar = document.getElementById('btn_cancelar');
+
+novoClienteBtn.addEventListener('click', openModal);
+closeModal.addEventListener('click', fechaModal);
+btnCancelar.addEventListener('click', fechaModal);
+window.addEventListener('click', outsideClick);
+
+function openModal() {
+    modalNovoCLiente.style.display ='block';
+}
+
+function fechaModal(x) {
+    if(x.target==closeModal || x.target==btnCancelar){
+        modalNovoCLiente.style.display = 'none';
+    }
+}
+
+function outsideClick(x) {
+    if (x.target == modalNovoCLiente){
+        modalNovoCLiente.style.display = 'none';
+    }
+
+}
+
+////////////////////////////////////////////////////
+
 $(document).ready(function(){
 
   var cliente_id;
